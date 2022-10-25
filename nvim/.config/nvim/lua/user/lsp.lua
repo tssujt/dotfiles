d334842baef90b4bea872b0781eec575f356862b
tssujt <github@tssujt.xyz>
2022-10-25 13:54:51 +0800

    [Neovim] cmp_nvim_lsp.update_capabilities is deprecated

--- nvim/.config/nvim/lua/user/lsp.lua
+++ nvim/.config/nvim/lua/user/lsp.lua
@@ -68,7 +68,7 @@ local nvim_status = require "lsp-status"
 
 require("user.statusline").activate()
 
-local updated_capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp
+local updated_capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp
     .protocol
     .make_client_capabilities())
 updated_capabilities.textDocument.codeLens = { dynamicRegistration = false }
