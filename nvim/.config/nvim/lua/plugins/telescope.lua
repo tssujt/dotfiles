8cbc375dbcc236a2d091b4a17adeb55b44125cb3
tssujt <github@tssujt.xyz>
2024-04-24 00:49:28 +0800

    [Neovim] Update settings for telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,10 +8,23 @@ return {
     "nvim-telescope/telescope-project.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make" },
   },
-  opts = function(_, opts)
+  opts = function()
     local actions = require "telescope.actions"
-    return require("astrocore").extend_tbl(opts, {
+    local get_icon = require("astroui").get_icon
+    return {
       defaults = {
+        git_worktrees = require("astrocore").config.git_worktrees,
+        prompt_prefix = get_icon("Selected", 1),
+        selection_caret = get_icon("Selected", 1),
+        path_display = { "truncate" },
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = { prompt_position = "top", preview_width = 0.55 },
+          vertical = { mirror = false },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
         mappings = {
           i = {
             ["<C-j>"] = actions.cycle_history_next,
@@ -52,10 +65,11 @@ return {
           },
         },
       },
-    })
+    }
   end,
   config = function(...)
-    -- require "plugins.configs.telescope"(...)
+    require "astronvim.plugins.configs.telescope"(...)
+
     local telescope = require "telescope"
     telescope.load_extension "fzf"
     telescope.load_extension "live_grep_args"
