46ed1f9c71ca19bb9f9a5534a6c3dada21e36842
tssujt <github@tssujt.xyz>
2024-04-24 00:48:01 +0800

    [Neovim] Update settings for lewis6991/gitsigns.nvim

--- nvim/.config/nvim/lua/plugins/community-git.lua
+++ nvim/.config/nvim/lua/plugins/community-git.lua
@@ -1,4 +1,3 @@
-local get_icon = require("astroui").get_icon
 return {
   {
     "NeogitOrg/neogit",
@@ -15,17 +14,19 @@ return {
     "lewis6991/gitsigns.nvim",
     enabled = vim.fn.executable "git" == 1,
     event = "User AstroGitFile",
-    opts = {
-      current_line_blame = true,
-      signs = {
-        add = { text = get_icon "GitSign" },
-        change = { text = get_icon "GitSign" },
-        delete = { text = get_icon "GitSign" },
-        topdelete = { text = get_icon "GitSign" },
-        changedelete = { text = get_icon "GitSign" },
-        untracked = { text = get_icon "GitSign" },
-      },
-      worktrees = vim.g.git_worktrees,
-    },
+    opts = function()
+      local get_icon = require("astroui").get_icon
+      return {
+        signs = {
+          add = { text = get_icon "GitSign" },
+          change = { text = get_icon "GitSign" },
+          delete = { text = get_icon "GitSign" },
+          topdelete = { text = get_icon "GitSign" },
+          changedelete = { text = get_icon "GitSign" },
+          untracked = { text = get_icon "GitSign" },
+        },
+        worktrees = require("astrocore").config.git_worktrees,
+      }
+    end,
   },
 }
