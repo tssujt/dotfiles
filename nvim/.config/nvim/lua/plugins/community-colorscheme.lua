bba3b91cd7a740e22ece9776ea223a051e8ac29b
tssujt <github@tssujt.xyz>
2024-12-09 17:31:53 +0800

    [Neovim] Refactor Neovim colorscheme config

---
+++ nvim/.config/nvim/lua/plugins/community-colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = false,
+    opts = {
+      italic_comments = true,
+      underline_links = true,
+      disable_nvimtree_bg = false,
+    },
+  },
+}
